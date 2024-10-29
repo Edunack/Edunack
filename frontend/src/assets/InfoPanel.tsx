f4c2b8b04c82342eefee0081857d73b470a2c289
lunaru100 <lunaruteyvat@gmail.com>
2024-10-29 10:52:36 +0100

    fixed main page
    Deleted import that seemed useless which apparently was useful so I had to refactor whole code

--- frontend/src/assets/InfoPanel.tsx
+++ frontend/src/assets/InfoPanel.tsx
@@ -18,23 +18,25 @@ function InfoPanel() {
           </clipPath>
         </defs>
       </svg>
-      <div id="howTo" className="sect">
-        <p>How does this work?</p>
-        <ul>
-          <li>You tell us what you want to learn</li>
-          <li>We show you the courses that people found most helpful</li>
-          <li>we show you where can you take the course</li>
-          <li>we will try to keep you motivated with our extension</li>
-        </ul>
-      </div>
-      <div id="catchphrase" className="sect">
-        <p>
-          HAVE YOU EVER WANTED TO LEARN A SKILL AND THERE WERE JUST TOO MANY
-          COURSES
-          <br />
-          /TUTORIALS?
-          <br /> <span id="wrapper">FIND THE BEST SUITED ONE ON EDUNACK</span>
-        </p>
+      <div id="infoTransform">
+        <div id="howTo" className="sect">
+          <p>How does this work?</p>
+          <ul>
+            <li>You tell us what you want to learn</li>
+            <li>We show you the courses that people found most helpful</li>
+            <li>we show you where can you take the course</li>
+            <li>we will try to keep you motivated with our extension</li>
+          </ul>
+        </div>
+        <div id="catchphrase" className="sect">
+          <p>
+            HAVE YOU EVER WANTED TO LEARN A SKILL AND THERE WERE JUST TOO MANY
+            COURSES
+            <br />
+            /TUTORIALS?
+            <br /> <span id="wrapper">FIND THE BEST SUITED ONE ON EDUNACK</span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
-import "./main.css"
+//import "bootstrap/dist/css/bootstrap.css";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
