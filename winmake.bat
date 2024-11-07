611ee79b9655cf1001ea0f8e466c65a699f6c652
Piotr Bartoszewicz <piotrek15.pb@gmail.com>
2024-11-07 18:34:46 +0100

    fix batch file

--- winmake.bat
+++ winmake.bat
@@ -1,16 +1,28 @@
-@ECHO OFF
-SET DATABASE_URL="sqlite://db.sqlite"
+@echo off
+set DATABASE_URL="sqlite://db.sqlite"
 
-if %1=="run" (
-    cd frontend
+if %~1=="" (
+    echo "Missing argument: 'run' or 'build'"
+    goto end
+)
+
+if %~1==run (
+    cd ./frontend
     cmd /c "npm run build"
     cd ../backend
     cmd /c "cargo run"
     cd ..
-) else if %1=="build" (
-    cd frontend
+    goto end
+)
+
+if %~1==build (
+    cd ./frontend
     cmd /c "npm run build"
     cd ../backend
     cmd /c "cargo build"
     cd ..
+    goto end
 )
+
+:end
+
