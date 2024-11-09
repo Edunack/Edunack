7d79412f4d5d4baff7a1e17364a77646910bb671
Piotr Bartoszewicz <piotrek15.pb@gmail.com>
2024-11-09 18:26:11 +0100

    updates

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,2 @@
-CREATE TABLE users(id blob(16) not null primary key, username text not null, email text not null, password text not null);
+-- users table
+CREATE TABLE IF NOT EXISTS users(id blob(16) not null primary key, username text not null, email text not null, password text not null);
