aecba97d0f7180a5cf4276f025be89be66cad40a
lunaru100 <lunaruteyvat@gmail.com>
2024-11-18 09:39:51 +0100

    searchbar crashes

--- frontend/src/routes/RankingPage/Search.tsx
+++ frontend/src/routes/RankingPage/Search.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react";
+import { useRef } from "react";
 import "./Search.css";
 
 interface Props {
@@ -6,9 +6,39 @@ interface Props {
 }
 
 function Search({ onSearch }: Props) {
-  const handleSubmit = (e: FormEvent) => {
+  let searchRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSearch();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    fetch(
+      "api/search/categories?" +
+        new URLSearchParams({ lang: "en", startsWith: "" + data.searchBar }),
+      {
+        method: "GET",
+      }
+    )
+      .then((data) => data.json())
+      .then(async (data) => {
+        console.log(data);
+
+        onSearch();
+      });
+  };
+
+  const handleKeyDown = () => {
+    const data = new String(searchRef.current?.value);
+
+    fetch(
+      "api/search/categories?" +
+        new URLSearchParams({ lang: "en", startsWith: "" + data }),
+      {
+        method: "GET",
+      }
+    ).then((data) => data.json());
   };
   return (
     <form id="search" onSubmit={handleSubmit}>
@@ -22,6 +52,8 @@ function Search({ onSearch }: Props) {
           name="searchBar"
           id="searchBar"
           placeholder="Type in the category"
+          onKeyDown={handleKeyDown}
+          ref={searchRef}
         />
         <button id="searchBtn" type="submit">
           SEARCH
